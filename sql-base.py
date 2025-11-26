570c6126efc5169942fc06e9d81ce2967fa38b1e
akindutire <akinsamuel33@gmail.com>
2025-11-26 17:25:29 +0100

    added database support

--- sql-base.py
+++ sql-base.py
@@ -1,1 +1,26 @@
-# research custom modules import 
+# Installation of mysql connector
+# pip install mysql-connector-python
+
+import mysql.connector as sql
+
+db = sql.connect(
+    host="localhost",
+    user="root",
+    password="mac_root",
+    database="sqi_l1"
+)
+
+print("Database connected successfully")
+
+cursor = db.cursor() # Create a cursor object to interact with the database
+
+# cursor.execute("show databases")
+# databases = cursor.fetchall()
+
+tables = cursor.execute("show tables")
+print("tables:", cursor.fetchall())
+
+#Queries
+#drop database sqi_l1
+#create database sqi_l1
+
