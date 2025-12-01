54ee975beb582fd00c772a93fd648b48d3bc1d60
akindutire <akinsamuel33@gmail.com>
2025-12-01 14:49:07 +0100

    bootstrapped orchester

--- cbt-project/index.py
+++ cbt-project/index.py
@@ -7,8 +7,14 @@ class CBTProject:
         self.__admin = Admin()
         
     def orchestrate(self):
+        print("Welcome to CBT CLI application...\n")
         self.__admin.bootstrap()
         # self.__student.register()
         # self.__student.take_test()
         # self.__student.show_results()
         
+
+# This implies that this file is being run directly
+if __name__ == "__main__":
+    app = CBTProject()
+    app.orchestrate()
